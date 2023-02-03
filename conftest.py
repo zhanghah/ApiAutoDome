1313376652027f3c4285f1e8eb33b76c308dbd63
zhanghah <1915579714@qq.com>
2023-02-03 09:26:28 +0800

    'update'

--- conftest.py
+++ conftest.py
@@ -1,3 +1,11 @@
+'''
+Author: zhanghah 1915579714@qq.com
+Date: 2023-01-31 14:37:25
+LastEditors: zhanghah 1915579714@qq.com
+LastEditTime: 2023-02-03 08:57:57
+FilePath: \ApiAutoDome\conftest.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import pytest
 
 from common.yaml_util import YamlUtil
